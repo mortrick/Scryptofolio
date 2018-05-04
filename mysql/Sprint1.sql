91742dd8b146e843e2b62f988eecd45cf4914559
mortrick <david.yafe@gmail.com>
2018-05-04 16:00:05 +0300

    chartsUpdate

---
+++ mysql/Sprint1.sql
@@ -0,0 +1,19 @@
+# Tables for charts
+
+
+create table charts_btc_10min (
+cur_Time timestamp,
+cur_Value float
+);
+
+
+create table charts_bcc_10min (
+cur_Time timestamp,
+cur_Value float
+);
+
+
+create table charts_eth_10min (
+cur_Time timestamp,
+cur_Value float
+);
